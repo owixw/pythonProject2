237997fea7bc71f53a00f275968f22db503083ac
Mariia Andrianova <owixw1@gmail.com>
2024-03-18 23:40:37 +0200

    Test for reading from file using Pandas

--- tests/tests_io/test_input.py
+++ tests/tests_io/test_input.py
@@ -20,3 +20,18 @@ def test_read_from_file_exception():
         read_from_file("Some_file")
 
 
+def test_format_read_from_file_using_pandas():
+    data = read_from_file_using_pandas("tests/data_test/username_test.csv")
+    assert isinstance(data, pd.DataFrame)
+
+
+def test_read_from_file_using_pandas():
+    actual_data = read_from_file_using_pandas("tests/data_test/username_test.csv")
+    expected_data = pd.DataFrame([["booker12", "Rachel", "Booker"], ["grey07", "Laura", "Grey"]],
+                                 columns=["Username", "Firstname", "Lastname"])
+    pd.testing.assert_frame_equal(actual_data, expected_data)
+
+
+def test_read_from_file_using_pandas_exception():
+    with pytest.raises(FileNotFoundError):
+        read_from_file("Some_file")
